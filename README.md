89c25a42022e2d1d97b0861b622982158a597a0c
batra98 <batragaurav2616@gmail.com>
2019-01-13 21:34:01 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# jet_joyride
+Graphics Boilerplate Code
+=========================
 
+This is boilerplate code for the assignments which might be helpful.
+
+
+License
+-------
+The MIT License https://meghprkh.mit-license.org/
+
+Copyright &copy; 2018 Megh Parikh <meghprkh@gmail.com>
---
+++ build/CMakeFiles/graphics_asgn1.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gaurav/Graphics/jet_joyride/src/ball.cpp" "/home/gaurav/Graphics/jet_joyride/build/CMakeFiles/graphics_asgn1.dir/src/ball.cpp.o"
+  "/home/gaurav/Graphics/jet_joyride/src/color.cpp" "/home/gaurav/Graphics/jet_joyride/build/CMakeFiles/graphics_asgn1.dir/src/color.cpp.o"
+  "/home/gaurav/Graphics/jet_joyride/src/input.cpp" "/home/gaurav/Graphics/jet_joyride/build/CMakeFiles/graphics_asgn1.dir/src/input.cpp.o"
+  "/home/gaurav/Graphics/jet_joyride/src/main.cpp" "/home/gaurav/Graphics/jet_joyride/build/CMakeFiles/graphics_asgn1.dir/src/main.cpp.o"
+  "/home/gaurav/Graphics/jet_joyride/src/nonedit.cpp" "/home/gaurav/Graphics/jet_joyride/build/CMakeFiles/graphics_asgn1.dir/src/nonedit.cpp.o"
+  "/home/gaurav/Graphics/jet_joyride/src/other_handlers.cpp" "/home/gaurav/Graphics/jet_joyride/build/CMakeFiles/graphics_asgn1.dir/src/other_handlers.cpp.o"
+  "/home/gaurav/Graphics/jet_joyride/src/timer.cpp" "/home/gaurav/Graphics/jet_joyride/build/CMakeFiles/graphics_asgn1.dir/src/timer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/libdrm"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
